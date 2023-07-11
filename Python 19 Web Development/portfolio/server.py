2f042719fb035f7f75744e4d16e39945fe7f35d1
Karthik Ram <karthikramx@Karthiks-MacBook-Pro.local>
2023-07-11 00:13:05 -0700

    changes

--- Python 19 Web Development/portfolio/server.py
+++ Python 19 Web Development/portfolio/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 # instantiating the flask application
 app = Flask(__name__)
@@ -12,10 +12,19 @@ def home():
     return render_template('index.html')
 
 @app.route('/<string:page_name>')
-def index(page_name):
+def page(page_name):
     return render_template(page_name)
 
 
+@app.route('/submit_form', methods=["POST","GET"])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'something went wrong'
 
 
-# add a favicon
+
+
